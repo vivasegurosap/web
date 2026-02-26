3904abc96712bc30a02ecb91f83d29da9cc7067d
vivasegurosap <tecnologiasvisuales940@gmail.com>
2026-02-26 15:19:08 -0500

    ajustes

--- app.py
+++ app.py
@@ -447,4 +447,5 @@ def estado(id, estado):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
